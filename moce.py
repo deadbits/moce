34fcc0cd1e2d9c620c51af56182b3ab82aeb8092
deadbits <adam@deadbits.org>
2023-12-31 15:28:04 -0500

    better

--- moce.py
+++ moce.py
@@ -1,11 +1,10 @@
 import os
+import json
 import chainlit as cl
 
 from embedchain import Pipeline as App
-from tinydb import TinyDB
-from tinydb.storages import JSONStorage
-from tinydb.middlewares import CachingMiddleware
 from datetime import datetime
+
 
 try:
     HF_TOKEN = os.environ['HUGGINGFACE_API_KEY']
@@ -15,34 +14,80 @@ except Exception as err:
     raise(err)
 
 
+class DatabaseError(Exception):
+    pass
+
+
+class JSONDB:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def _create_file_if_not_exists(self):
+        if not os.path.exists(self.file_path):
+            with open(self.file_path, 'w') as fp:
+                json.dump([], fp)
+
+    def add_record(self, record):
+        try:
+            self._create_file_if_not_exists()
+
+            with open(self.file_path, 'r+') as fp:
+                try:
+                    data = json.load(fp)
+
+                    if record not in data:
+                        data.append(record)
+                    else:
+                        pass
+                except Exception as err:
+                    print(f'[DEBUG] Error adding record: {str(err)}')
+                    raise(err)
+
+                fp.seek(0)
+                json.dump(data, fp, indent=4)
+
+        except (FileNotFoundError, json.JSONDecodeError, IOError) as e:
+            raise DatabaseError(f"Error adding record: {str(e)}")
+
+    def get_all_records(self):
+        try:            
+            with open(self.file_path, 'r') as fp:
+                # Attempt to load data, handle empty file scenario
+                try:
+                    data = json.load(fp)
+                except json.JSONDecodeError:
+                    data = []
+
+            return data
+
+        except (FileNotFoundError, IOError) as e:
+            raise DatabaseError(f"Error getting all records: {str(e)}")
+
+    def get_top_records(self, n):
+        try:
+            records = self.get_all_records()
+            sorted_records = sorted(records, key=lambda x: x.get('added', 0), reverse=True)
+            return sorted_records[:n]
+        except (FileNotFoundError, json.JSONDecodeError, IOError) as e:
+            raise DatabaseError(f"Error getting top records: {str(e)}")
+
+
 @cl.on_chat_start
 async def setup_app():
     app = App.from_config(config_path='data/config.yaml')
     app.collect_metrics = False
     cl.user_session.set('app', app)
-
-    db = TinyDB('data/index.json',
-        storage=CachingMiddleware(JSONStorage)
-    )
-    db_table = db.table('index')
-    cl.user_session.set('jdb', db_table)
+    db = JSONDB('data/index.json')
+    cl.user_session.set('db', db)
 
 
 def update_db(data):
-    db = cl.user_session.get('jdb')
-    db.insert({
-        'document': data,
-        'added': datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    })
-
-
-def get_last_added():
-    db = cl.user_session.get('jdb')
-    data = db.all()
-    print(f'DEBUG: KB: {data}')
-    reversed_entries = list(reversed(list(data.items())))
-    result = dict(reversed_entries[:25])
-    return result
+    db = cl.user_session.get('db')
+    record = {
+        'url': data,  # Store the URL as a JSON field
+        'added': datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    }
+    db.add_record(record)
 
 
 @cl.on_message
@@ -54,32 +99,53 @@ async def main(message: cl.Message):
     msg = cl.Message(content='')
 
     user_message = message.content
-    print(user_message)
 
     if user_message.startswith('/add'):
+        data = user_message.replace('/add', '').strip()
+        db = cl.user_session.get('db')
 
-        add_task = cl.Task(title='Adding to knowledge base', status=cl.TaskStatus.RUNNING)
-        await task_list.add_task(add_task)
-        await task_list.send()
+        records = db.get_all_records()
+        if data in [record['url'] for record in records]:
+            await cl.Message(
+                content='This document already exists in the knowledge base!'
+            ).send()
+        else:
+            add_task = cl.Task(title='Adding to knowledge base', status=cl.TaskStatus.RUNNING)
+            await task_list.add_task(add_task)
+            await task_list.send()
 
-        data = user_message.replace('/add', '')
-        app.add(data)
-    
-        add_task.status = cl.TaskStatus.DONE
-        await task_list.send()
+            app.add(data)
+            update_db(data)
+            add_task.status = cl.TaskStatus.DONE
+            await task_list.send()
 
-        await cl.Message(
-            content='Added data to knowledge base!'
-        ).send()
+            await cl.Message(
+                content='Added data to knowledge base!'
+            ).send()
 
     elif user_message.startswith('/kb'):
         kb_task = cl.Task(title='Getting records', status=cl.TaskStatus.RUNNING)
         await task_list.add_task(kb_task)
         await task_list.send()
 
-        data = get_last_added()
+        data = cl.user_session.get('db').get_top_records(25)
         kb_task.status = cl.TaskStatus.DONE
         await task_list.send()
+
+        if len(data) == 0:
+            await cl.Message(
+                content='No documents in json index!'
+            ).send()
+
+        else:
+            markdown_content = "| URL | Added |\n| --- | --- |\n"
+            for record in data:
+                url = record['url']
+                added = record['added']
+                markdown_content += f"| {url} | {added} |\n"
+            await cl.Message(
+                content=markdown_content
+            ).send()
 
     else:
         chat_task = cl.Task(title='Querying LLM', status=cl.TaskStatus.RUNNING)
